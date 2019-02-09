d8b827c7352913b552752e89cd19978f1fcf8314
harrisaz <35793456+harrisaz@users.noreply.github.com>
2019-02-09 16:17:44 -0700

    Original uvm1.2/examples/integrated
    removed ./ubus (not used) and top-level README.txt. Added ./edaplayground/testbench.sv.

---
+++ edaplayground/testbench.sv
@@ -0,0 +1,4 @@
+// Code your testbench here
+// or browse Examples
+`include "tb_top.sv"
+`include "test.sv"
---
+++ sim/files.f
@@ -0,0 +1,9 @@
++incdir+${UVM_HOME}
++incdir+${PROJECT_LOC}/apb
++incdir+${PROJECT_LOC}/codec
++incdir+${PROJECT_LOC}/vip
++incdir+${PROJECT_LOC}/dut
++incdir+${PROJECT_LOC}/tb/sv
++incdir+${PROJECT_LOC}/tb/tests
+
+${PROJECT_LOC}/tb/tests/test.sv
